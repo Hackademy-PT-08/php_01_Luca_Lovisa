949373628f0efff9a3ddd245612f1eb66edce5fb
Bartimeus88 <luca.lovisa8@gmail.com>
2023-06-27 19:46:20 +0200

    php 1 = Esercizio 1 e 2

---
+++ php_1.php
@@ -0,0 +1,69 @@
+<?php
+//
+/* 
+Traccia 1:
+
+Definisci 4 variabili:
+
+    - Integer 
+    - Float 
+    - String 
+    - Boolean
+
+A schermo, fai comparire il tipo di dato delle varie variabili.
+Trasforma quelle variabili in costanti, utilizzando le best practice viste a lezione.
+
+*/
+
+// $integer = 10;
+// $float = 5.5;
+// $string = "stringa";
+// $boolean = false;
+
+// echo $integer."\n".$float."\n".$string."\n".$boolean;
+// // echo $integer."\n";
+// // echo $float."\n";
+// // echo $string."\n";
+// // echo $boolean;
+
+// const INTEGER = 10;
+// const FLOAT = 5.5;
+// const STRING = "stringa";
+// const BOOLEAN = false;
+
+// echo INTEGER
+
+
+
+/*
+
+- Traccia 2:
+
+Date le seguenti variabili:
+
+    $1text = "Marco";
+    $text2 = "hai";
+    $text.3 = "sete";
+    $text4 = "?";
+    @text5 = "Perchè";
+    $te-xt6 = '$text2';
+    $text 7 = 'bevuto';
+    $text8 = "tutto"
+
+correggi eventuali errori e stampa correttamente a terminale la stringa: “Marco Hai sete? Perche' hai bevuto tutto.“
+*/
+
+// $text1 = "Marco ";
+// $text2 = "hai ";
+// $text_3 = "sete";
+// $text4 = "?";
+// $text5 = " Perchè";
+// $te_xt6 = ' hai';
+// $text_7 = ' bevuto';
+// $text8 = " tutto.";
+
+// echo ($text1.$text2.$text_3.$text4.$text5.$te_xt6.$text_7.$text8)
+
+
+
+?>
